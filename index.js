93267d36749da68a05b081d7085c686182415471
Tumin Sheth <tumin.sheth102987@marwadiuniversity.ac.in>
2020-06-18 02:08:01 +0530

    done

--- index.js
+++ index.js
@@ -14,5 +14,13 @@ app.use(express.static('public'));
 //Socket Setup
 var io = socket(server);
 io.on('connection', (socket) => {
-    console.log('Socket Connection ' + socket);
-})
+    console.log('Socket Connection ' , socket.id);
+
+    socket.on('chat', (data) => {
+        io.sockets.emit('chat', data);
+    });
+
+    socket.on('typing', (data) => {
+        socket.broadcast.emit('typing', data)
+    })
+});
--- public/chat.js
+++ public/chat.js
@@ -1,3 +1,32 @@
-//Make connection
+// Make connection
+var socket = io.connect('http://localhost:3000');
 
-var socket = io.connect('http://localhost:3000')
+// Query DOM
+var message = document.getElementById('message'),
+      username = document.getElementById('username'),
+      btn = document.getElementById('send'),
+      output = document.getElementById('output'),
+      feedback = document.getElementById('feedback');
+
+// Emit events
+btn.addEventListener('click', function(){
+    socket.emit('chat', {
+        message: message.value,
+        username: username.value
+    });
+    message.value = "";
+});
+
+message.addEventListener('keypress', function(){
+    socket.emit('typing', username.value);
+})
+
+// Listen for events
+socket.on('chat', function(data){
+    feedback.innerHTML = '';
+    output.innerHTML += '<p><strong>' + data.username + ': </strong>' + data.message + '</p>';
+});
+
+socket.on('typing', function(data){
+    feedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>';
+});
